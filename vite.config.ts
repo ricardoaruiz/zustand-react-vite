42861c36efd64032555f1aba708bd82259814963
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-02-27 15:26:02 -0300

    chore: adding roboto google font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import webfontDownload from 'vite-plugin-webfont-dl';
+import { ViteMinifyPlugin as minify } from 'vite-plugin-minify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+    ]),
+    minify({}),
+    splitVendorChunkPlugin(),
+  ],
 })
